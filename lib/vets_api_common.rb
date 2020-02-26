d8386db8c5f5d94b39dd461cfac820148f39732d
Philip Becker <philip.becker@oddball.io>
2020-02-26 14:21:10 -0500

    models/base spec passes

--- lib/vets_api_common.rb
+++ lib/vets_api_common.rb
@@ -1,12 +1,16 @@
 require "vets_api_common/version"
-require 'common/convert_to_pdf'
-require 'common/virus_scan'
-require 'common/file_helpers'
-require 'common/event_rate_limiter'
-require 'common/hash_helpers'
+
 require 'redis'
 require 'redis-namespace'
 require 'ostruct'
 require 'mini_magick'
 require 'carrierwave'
+require 'active_model'
 require 'action_dispatch'
+
+require 'common/convert_to_pdf'
+require 'common/virus_scan'
+require 'common/file_helpers'
+require 'common/event_rate_limiter'
+require 'common/hash_helpers'
+require 'common/exceptions/serializable_error'
